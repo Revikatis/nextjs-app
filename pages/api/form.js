08da6e50e088d06cc806105d5a8f224886364fc4
Wojciech Wolanin <arc.revikatis@gmail.com>
2023-06-30 17:31:10 +0200

    zad10

---
+++ pages/api/form.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+
+  const body = req.body;
+
+  console.log('body: ', body);
+ 
+  if (!body.name || !body.email) {
+
+    return res.status(400).json({ data: 'name or e-mail not found' });
+  }
+ 
+  res.status(200).json({ data: `${body.name} ${body.email}` });
+}
---
+++ pages/form.js
@@ -0,0 +1,47 @@
+// index.html
+import { useState } from 'react';
+import Link from 'next/link';
+
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+
+export default function Form() {
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = '/api/form';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSONdata,
+    };
+    const response = await fetch(endpoint, options);
+
+    const result = await response.json();
+    alert(`Your name and e-mail: ${result.data}`);
+  };
+  return (
+  <div>
+    <a href="/">index</a>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" required />
+ 
+      <label htmlFor="email">e-mail</label>
+      <input type="text" id="email" name="email" required />
+ 
+      <button type="submit">Submit</button>
+    </form>
+	</div>
+  );
+
+}
